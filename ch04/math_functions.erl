c1f98bff31daebe7fc0316b73c7ee63515b90291
Patrick Bucher <patrick.bucher@mailbox.org>
2023-12-31 13:24:14 +0100

    chapter 4: exercise 7

--- ch04/math_functions.erl
+++ ch04/math_functions.erl
@@ -1,7 +1,26 @@
 -module(math_functions).
--export([even/1, odd/1, filter/2]).
+-export([even/1, odd/1, filter/2, split/1, splitacc/1]).
 
 even(X) -> X rem 2 == 0.
 odd(X) -> X rem 2 == 1.
 
 filter(F, L) -> [X || X <- L, F(X)].
+
+split([]) ->
+    [[],[]];
+split(L) ->
+    {filter(fun(X) -> even(X) end, L),
+     filter(fun(X) -> odd(X) end, L)}.
+
+splitacc([]) ->
+    [[], []];
+splitacc(L) ->
+    splitacc(L, {[], []}).
+
+splitacc([], {Even, Odd}) ->
+    {lists:reverse(Even), lists:reverse(Odd)};
+splitacc([H|T], {Even, Odd}) ->
+    case (H rem 2) of
+        0 -> splitacc(T, {[H|Even], Odd});
+        1 -> splitacc(T, {Even, [H|Odd]})
+    end.
