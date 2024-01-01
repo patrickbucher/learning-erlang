728dec2cc1f22ac7efaad8a62b6ac6c4d20053ce
Patrick Bucher <patrick.bucher@mailbox.org>
2024-01-01 13:03:33 +0100

    chapter 5: exercise 3

--- ch05/my_maps.erl
+++ ch05/my_maps.erl
@@ -1,5 +1,5 @@
 -module(my_maps).
--export([map_search_pred/2]).
+-export([map_search_pred/2, merge/2]).
 
 map_search_pred(Map, Pred) ->
     List = maps:to_list(Map),
@@ -12,3 +12,15 @@ map_search_pred(Map, Pred) ->
 % > BothOdd = fun(K, V) -> K rem 2 =:= 1 andalso V rem 2 =:= 1 end.
 % > my_maps:map_search_pred(Squares, BothOdd).
 % #{1 => 1}
+
+merge(A, B) ->
+    maps:from_list(maps:to_list(B) ++ maps:to_list(A)).
+
+% usage:
+% > c(my_maps).
+% > Authors = #{armstrong => "Programming Erlang", kernighan => "The C Programming Language"}.
+% > Programmers = #{armstrong => "Erlang", ritchie => "C", hickey => "Clojure"}.
+% > my_maps:merge(Authors, Programmers).
+% #{armstrong => "Programming Erlang",
+%  kernighan => "The C Programming Language",ritchie => "C",
+%  hickey => "Clojure"}
