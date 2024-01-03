c215f9d049cedd5a1ef6c614e86906e7c2988d2f
Patrick Bucher <patrick.bucher@mailbox.org>
2024-01-03 09:31:33 +0100

    chapter 7: exercise 2

--- ch07/bitfiddle.erl
+++ ch07/bitfiddle.erl
@@ -1,5 +1,5 @@
 -module(bitfiddle).
--export([reverse_bytes/1]).
+-export([reverse_bytes/1, binary_to_packet/1]).
 
 % usage:
 % > bitfiddle:reverse_bytes(<<"abcdef">>).
@@ -10,3 +10,11 @@ reverse_bytes(Bytes) when is_binary(Bytes) ->
     ByteList = [ X || <<X:8>> <= Bytes ],
     Reversed = lists:reverse(ByteList),
     << <<X>> || X <- Reversed >>.
+
+% usage:
+% > bitfiddle:binary_to_packet("abcdef").
+% <<0,0,0,10,131,107,0,6,97,98,99,100,101,102>>
+binary_to_packet(Term) ->
+    Payload = term_to_binary(Term),
+    N = byte_size(Payload),
+    <<N:(4*8), Payload:N/binary>>.
