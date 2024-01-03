bef43249ac6a36c965b55265a79ee59ca6d2c412
Patrick Bucher <patrick.bucher@mailbox.org>
2024-01-03 12:00:56 +0100

    decompress text using huffman encoding tree

--- huffman/huffman.erl
+++ huffman/huffman.erl
@@ -1,9 +1,21 @@
 -module(huffman).
--export([compress/1]).
+-export([compress/1, decompress/2]).
 
-compress(String) ->
-    Tree = build_huffman_tree(String),
-    Payload = compress(list_to_binary(String), Tree, <<>>),
+decompress(Payload, Tree) ->
+    decode(Payload, Tree, Tree, <<>>).
+
+decode(<<>>, _, {C, _, _, _}, Acc) -> <<Acc/binary, C/binary>>;
+decode(Payload, Tree, {C, _, {}, {}}, Acc) ->
+    decode(Payload, Tree, Tree, <<Acc/binary, C/binary>>);
+decode(<<Step:1, Rest/bitstring>>, Tree, {_, _, L, R}, Acc) ->
+    case Step of
+        0 -> decode(Rest, Tree, L, Acc);
+        1 -> decode(Rest, Tree, R, Acc)
+    end.
+
+compress(Bin) ->
+    Tree = build_huffman_tree(Bin),
+    Payload = compress(Bin, Tree, <<>>),
     {Payload, Tree}.
 
 compress(<<>>, _, Acc) -> Acc;
@@ -16,13 +28,13 @@ encode(C, {_, _, {LC, _, _, _} = L, {RC, _, _, _} = R}, Acc) ->
     Left = lists:member(C, binary_to_list(LC)),
     Right = lists:member(C, binary_to_list(RC)),
     if
-        Left -> encode(C, L, <<Acc/bitstring, 1:1>>);
-        Right -> encode(C, R, <<Acc/bitstring, 0:1>>);
+        Left -> encode(C, L, <<Acc/bitstring, 0:1>>);
+        Right -> encode(C, R, <<Acc/bitstring, 1:1>>);
         true -> {error, io_lib:format("~s not found in tree", C)}
     end.
 
-build_huffman_tree(String) ->
-    Freqs = freq_asc(String),
+build_huffman_tree(Bin) ->
+    Freqs = freq_asc(Bin),
     Nodes = lists:map(fun({C, F}) -> {C, F, {}, {}} end, Freqs),
     hd(build_tree(Nodes)).
 
@@ -33,8 +45,8 @@ build_tree([{CA, FA, _, _} = L, {CB, FB, _, _} = R | T]) ->
     SortedTree = lists:sort(fun({_, LF, _, _}, {_, RF, _, _}) -> LF < RF end, Tree),
     build_tree(SortedTree).
 
-freq_asc(String) ->
-    Chars = [ X || X <- String ],
+freq_asc(Bin) ->
+    Chars = [ X || X <- binary_to_list(Bin) ],
     FreqMap = lists:foldl(fun map_freq/2, maps:new(), Chars),
     FreqList = maps:to_list(FreqMap),
     lists:sort(fun({_, LF}, {_, RF}) -> LF < RF end, FreqList).
