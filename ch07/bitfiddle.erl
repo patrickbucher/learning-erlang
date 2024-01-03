df5d32f1229f8b9bd0f423a99ea06decabb6fb75
Patrick Bucher <patrick.bucher@mailbox.org>
2024-01-03 09:39:39 +0100

    chapter 7: exercises 3 & 4

--- ch07/bitfiddle.erl
+++ ch07/bitfiddle.erl
@@ -1,5 +1,6 @@
 -module(bitfiddle).
--export([reverse_bytes/1, binary_to_packet/1]).
+-export([reverse_bytes/1, binary_to_packet/1, packet_to_term/1,
+         test_reverse_bytes/0, test_packet/0]).
 
 % usage:
 % > bitfiddle:reverse_bytes(<<"abcdef">>).
@@ -18,3 +19,30 @@ binary_to_packet(Term) ->
     Payload = term_to_binary(Term),
     N = byte_size(Payload),
     <<N:(4*8), Payload:N/binary>>.
+
+% usage:
+% > bitfiddle:packet_to_term(<<0,0,0,10,131,107,0,6,97,98,99,100,101,102>>).
+% "abcdef"
+packet_to_term(Packet) when is_bitstring(Packet) ->
+    <<_:(4*8), Payload/binary>> = Packet,
+    binary_to_term(Payload).
+
+% usage:
+% > bitfiddle:test_reverse_bytes().
+% ok
+test_reverse_bytes() ->
+    Original = <<"hello, world">>,
+    Reversed = reverse_bytes(Original),
+    ReReversed = reverse_bytes(Reversed),
+    ReReversed = Original,
+    ok.
+
+% usage:
+% > bitfiddle:test_packet().
+% ok
+test_packet() ->
+    Term = {multiplication, 2, 3, 6},
+    Packet = binary_to_packet(Term),
+    Unpacked = packet_to_term(Packet),
+    Term = Unpacked,
+    ok.
