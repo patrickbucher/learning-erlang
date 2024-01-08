5f972c15b81081aa2e737e997647d052c6cc8097
Patrick Bucher <patrick.bucher@mailbox.org>
2024-01-08 21:32:59 +0100

    Go/Erlang: further simplified code

--- ch12/ring.erl
+++ ch12/ring.erl
@@ -2,34 +2,37 @@
 -export([benchmark/3, forward/3]).
 
 benchmark(N, M, Debug) ->
+    % register a controller process as final node
     case whereis(controller) of
         undefined -> register(controller, self());
         _ -> ok
     end,
+
+    % create and run n nodes
     NodeIDs = lists:seq(0, N),
     Successors = lists:zip(NodeIDs, tl(NodeIDs), {pad, {N + 1, 0}}),
-    SuccessorPids =
-    lists:foldr(
-      fun({L, R}, Acc) ->
-              case R of
-                  0 ->
-                      [{L, self()} | Acc];
-                  _ ->
-                      [{_, Succ} | _] = Acc,
-                      [{L, spawn(ring, forward, [L, Succ, Debug])} | Acc]
-              end
-      end, [], Successors),
-    PidsByIndex = maps:from_list(SuccessorPids), 
+    PidPairs = lists:foldr(fun(Pair, Acc) -> connect(Pair, Acc, Debug) end, [], Successors),
+
+    % initiate the ring call
+    {_ , First} = hd(PidPairs),
     statistics(wall_clock),
-    lists:foreach(fun(I) ->
-                          Message = io_lib:format("hello ~b", [I]),
-                          maps:get(0, PidsByIndex) ! Message
-                  end, lists:seq(1, M)),
+    lists:foreach(fun(I) -> ring_call(First, I) end, lists:seq(1, M)),
+
+    % drain the inbox and measure time
     receive
         _ ->
             {_, Time} = statistics(wall_clock),
             io:format("forwarded ~b messages ~b times in ~fs~n", [M, N, Time / 1000])
     end.
+
+ring_call(Pid, I) ->
+    Pid ! io_lib:format("hello ~b", [I]).
+
+connect({L, 0}, Acc, _) ->
+    [{L, self()} | Acc];
+connect({L, _}, Acc, Debug) ->
+    [{_, Succ} | _] = Acc,
+    [{L, spawn(ring, forward, [L, Succ, Debug])} | Acc].
 
 forward(ID, Succ, Debug) ->
     receive
--- ch12/ring.go
+++ ch12/ring.go
@@ -21,34 +21,41 @@ func main() {
 }
 
 func benchmark(n int, m int, debug bool) {
+	// create n nodes
 	nodes := make([]*Node, 0)
 	for i := 0; i < n; i++ {
 		nodes = append(nodes, &Node{i, make(chan string), nil})
 	}
+
+	// connect the nodes
 	for i := 0; i < n; i++ {
 		node := nodes[i]
 		if i < n-1 {
 			node.Succ = nodes[i+1]
 		}
 	}
-	start := time.Now()
+
+	// run the nodes in separate Goroutines
 	for i := 0; i < n-1; i++ {
 		go forward(nodes[i], debug)
 	}
+
+	// initiate the ring call
+	start := time.Now()
 	go func(ch chan<- string) {
 		for j := 0; j < m; j++ {
 			ch <- fmt.Sprintf("hello %d", j)
 		}
 	}(nodes[0].Chan)
+
+	// drain all the channels and measure time
 	for j := 0; j < m; j++ {
 		message := <-nodes[n-1].Chan
 		if debug {
 			fmt.Printf("'%s' forwarded from %d to %d\n", message, n-2, n-1)
 		}
 	}
-	end := time.Now()
-	diff := end.Sub(start)
-	fmt.Printf("forwarded %d messages %d times in %v\n", m, n, diff)
+	fmt.Printf("forwarded %d messages %d times in %v\n", m, n, time.Since(start))
 }
 
 func forward(node *Node, debug bool) {
