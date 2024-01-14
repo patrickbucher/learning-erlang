9d449838c1ff9365e56f2b0e7b17500b55e264fc
Patrick Bucher <patrick.bucher@mailbox.org>
2024-01-14 09:27:01 +0100

    chapter 13: exercise 5

--- ch13/multirespawn.erl
+++ ch13/multirespawn.erl
@@ -1,11 +1,27 @@
 -module(multirespawn).
--export([spawn_canaries/1]).
+-export([spawn_canaries/1, keep_alive/1]).
 
-% TODO: implement keep_alive for indexed pid map
+keep_alive(PidsIndexed) ->
+    spawn(fun() ->
+                  Refs = maps:map(fun(Pid, _) -> monitor(process, Pid) end, PidsIndexed),
+                  resurrection_loop(PidsIndexed, Refs)
+          end).
+
+resurrection_loop(PidsIndexed, Refs) ->
+    receive
+        {'DOWN', _, process, Pid, _} ->
+            Index = maps:get(Pid, PidsIndexed),
+            io:format("resurrect canary ~b~n", [Index]),
+            NewPid = spawn(fun() -> canary(Index) end),
+            NewRefs = [monitor(process, NewPid) | Refs],
+            Others = maps:without([Pid], PidsIndexed),
+            NewPidsIndexed = Others#{NewPid => Index},
+            resurrection_loop(NewPidsIndexed, NewRefs)
+    end.
 
 spawn_canaries(N) ->
-    IndexedPids = [{I, spawn(fun() -> canary(I) end)} || I <- for(0, N, [], fun(_, J) -> J end)],
-    maps:from_list(IndexedPids).
+    PidsIndexed = [{spawn(fun() -> canary(I) end), I} || I <- for(0, N, [], fun(_, J) -> J end)],
+    maps:from_list(PidsIndexed).
 
 for(N, N, Acc, _) -> lists:reverse(Acc);
 for(I, N, Acc, Next) -> for(I + 1, N, [Next(Acc, I) | Acc], Next).
