9391e0b539379019c9513a28af01a6bdc93738cc
Patrick Bucher <patrick.bucher@mailbox.org>
2024-01-15 20:12:04 +0100

    chapter 13 [WIP]: exercise 6

---
+++ ch13/respawnall.erl
@@ -0,0 +1,37 @@
+-module(respawnall).
+-export([spawn_canaries/1, keep_alive/1]).
+
+keep_alive(PidsIndexed) ->
+    spawn(fun() ->
+                  maps:map(fun(Pid, _) -> monitor(process, Pid) end, PidsIndexed),
+                  resurrection_loop(PidsIndexed)
+          end).
+
+resurrection_loop(PidsIndexed) ->
+    receive
+        {'DOWN', _, process, _, _} ->
+            io:format("resurrect all canaries~n"),
+            io:format("PidsIndexed: ~p~n", [PidsIndexed]),
+            NewPidsIndexed = maps:map(fun(I) ->
+                                              {spawn(fun() -> canary(I) end), I}
+                                      end, maps:values(PidsIndexed)),
+            lists:each(fun({Pid, _}) -> monitor(process, Pid) end, NewPidsIndexed),
+            resurrection_loop(NewPidsIndexed)
+    end.
+
+spawn_canaries(N) ->
+    PidsIndexed = [{spawn(fun() -> canary(I) end), I} || I <- for(0, N, [], fun(_, J) -> J end)],
+    maps:from_list(PidsIndexed).
+
+for(N, N, Acc, _) -> lists:reverse(Acc);
+for(I, N, Acc, Next) -> for(I + 1, N, [Next(Acc, I) | Acc], Next).
+
+canary(X) ->
+    receive
+        die ->
+            io:format("Canary ~b is dying.~n", [X])
+    after
+        5000 ->
+            io:format("Canary ~b is alive.~n", [X]),
+            canary(X)
+    end.
